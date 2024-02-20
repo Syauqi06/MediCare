74618903e00ecc1c3dfe1a2d59304de540d5f9a5
Syauqi06 <syauqi0066@gmail.com>
2024-02-20 11:42:37 +0700

    fill black

--- resources/views/pendaftaran/index.blade.php
+++ resources/views/pendaftaran/index.blade.php
@@ -17,11 +17,11 @@
                             </span>
                             <a href="rekam/cetak">
                                 <btn class="btn btn-success ml-auto">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                                    <svg style="fill: #ffff" xmlns="http://www.w3.org/2000/svg" height="1em"
                                         viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                                         <style>
                                             svg {
-                                                fill: #ffffff
+                                                fill: black
                                             }
                                         </style>
                                         <path
