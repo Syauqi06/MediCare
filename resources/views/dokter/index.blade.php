b586cf0c15c83177614ea88e776307c1027f309d
Gilang Rahma Hardiansyah <gilanghardiansyah49@gmail.com>
2024-02-22 07:23:24 +0700

    prubahan

--- resources/views/dokter/index.blade.php
+++ resources/views/dokter/index.blade.php
@@ -56,9 +56,10 @@
                                         <td>{{ $d->jenis_poli }}</td>
                                         <td>{{ $d->no_telp }}</td>
                                         <td>
-                                            @if ($d->foto_dokter)
-                                                <img src="{{ url('foto') . '/' . $d->foto_dokter }} "
-                                                    style="max-width: 150px; height: auto;" />
+                                            @if (!empty($imageDataArray[$loop->index]))
+                                                <img src="{{ $imageDataArray[$loop->index]['src'] }}"
+                                                    alt="{{ $imageDataArray[$loop->index]['alt'] }}"
+                                                    style="max-width: 100px; height: auto;" />
                                             @endif
                                         </td>
                                         <td>
